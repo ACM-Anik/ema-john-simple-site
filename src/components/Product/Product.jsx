dbbbbc6ab613ef0f99a13318fa57916a4f0ba531
ACM-Anik <anikmojumder5252@gmail.com>
2023-03-27 20:05:37 +0600

    font-awesome added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = (props) => {
     // console.log(props.product);
     const { img, name, price, seller, ratings, quantity } = props.product;
-    
+
     const handleAddToCart = props.handleAddToCart;
-    
+
 
     return (
         <div className='product'>
@@ -17,7 +19,10 @@ const Product = (props) => {
                 <p>Manufacturer : {seller}</p>
                 <p><small>Rating: {ratings} stars</small></p>
             </div>
-            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>Add To Cart</button>
+            <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
+                Add To Cart  
+                <FontAwesomeIcon icon={faShoppingCart}/>
+                </button>
         </div>
     );
 };
